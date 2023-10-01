b5170f09b74451085247b4f9d22990ff59c171c1
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-01 19:17:14 +0200

    Pierwsze zmienne

--- src/Variables/Variables.java
+++ src/Variables/Variables.java
@@ -2,9 +2,22 @@ package Variables;
 
 public class Variables {
     public static void main(String[] args) {
-        System.out.println("Jan Kowalski ma 25 lat");
-        System.out.println("Pan Kowalski ma pesel 123456789");
-        System.out.println("Jan ma wzrost 180 cm");
-        System.out.println("Jeżeli Jan urośnie o 5cm to będzie miał 185cm");
+        String firstName;
+        String lastName;
+        int age;
+        double height;
+
+        firstName = "Jan";
+        lastName = "Kowalski";
+        age = 25;
+        height = 180;
+
+
+
+        System.out.println(firstName + " " + lastName + " ma " + age + " lat.");
+        System.out.println("Pan " + lastName + " ma pesel 123456789");
+        System.out.println(firstName +" ma wzrost " + height + "cm");
+        System.out.println("Jeżeli " + firstName +  " urośnie o 5cm to będzie miał 185cm");
+
     }
 }
