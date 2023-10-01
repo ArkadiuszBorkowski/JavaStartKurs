9851795d3161797050116070b318d9b5d27f2ebf
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-01 19:10:21 +0200

    Lekcja - zmienne

---
+++ src/Variables/Variables.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        System.out.println("Jan Kowalski ma 25 lat");
+        System.out.println("Pan Kowalski ma pesel 123456789");
+        System.out.println("Jan ma wzrost 180 cm");
+        System.out.println("Jeżeli Jan urośnie o 5cm to będzie miał 185cm");
+    }
+}
