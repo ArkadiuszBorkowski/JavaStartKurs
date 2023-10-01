584314eeeddc387895eefede613904607cc132f9
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-01 19:18:49 +0200

    Zmienne finalne

--- src/Variables/Variables.java
+++ src/Variables/Variables.java
@@ -6,18 +6,22 @@ public class Variables {
         String lastName;
         int age;
         double height;
+        final String pesel;
+
 
         firstName = "Jan";
         lastName = "Kowalski";
         age = 25;
         height = 180;
+        pesel = "123456789";
+
 
 
 
         System.out.println(firstName + " " + lastName + " ma " + age + " lat.");
-        System.out.println("Pan " + lastName + " ma pesel 123456789");
+        System.out.println("Pan " + lastName + " ma pesel " + pesel);
         System.out.println(firstName +" ma wzrost " + height + "cm");
-        System.out.println("Jeżeli " + firstName +  " urośnie o 5cm to będzie miał 185cm");
+        System.out.println("Jeżeli " + firstName +  " urośnie o 5 cm to będzie miał 185cm");
 
     }
 }
