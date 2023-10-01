57f085911f5006badfa890c50461d3497cd07fa7
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-01 19:27:50 +0200

    Wykorzystanie zmiennch z użyciem funkcji Scenner input - wprowadzanie danych z klawiatury

--- src/Variables/Variables.java
+++ src/Variables/Variables.java
@@ -1,27 +1,41 @@
 package Variables;
 
+import java.util.Scanner;
+
 public class Variables {
     public static void main(String[] args) {
+
         String firstName;
         String lastName;
         int age;
         double height;
         final String pesel;
 
+        Scanner input = new Scanner(System.in);
 
-        firstName = "Jan";
-        lastName = "Kowalski";
-        age = 25;
-        height = 180;
-        pesel = "123456789";
+        System.out.println("Wprowadź swoje imię");
+        firstName = input.nextLine();
 
+        System.out.println("Wprowadź swoje nazwisko");
+        lastName = input.nextLine();
+
+        System.out.println("Wprowadź swój wiek");
+        age = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Wprowadź swój pesel");
+        pesel = input.nextLine();
+
+        System.out.println("Wprowadź swój wzrost");
+        height = input.nextDouble();
 
 
 
         System.out.println(firstName + " " + lastName + " ma " + age + " lat.");
-        System.out.println("Pan " + lastName + " ma pesel " + pesel);
+
+        System.out.println(lastName + " ma pesel " + pesel);
         System.out.println(firstName +" ma wzrost " + height + "cm");
-        System.out.println("Jeżeli " + firstName +  " urośnie o 5 cm to będzie miał 185cm");
+        System.out.println("Jeżeli " + firstName +  " urośnie o 5 cm to będzie miał " + (height + 5) + "cm");
 
     }
 }
