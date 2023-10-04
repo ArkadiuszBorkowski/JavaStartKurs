e2479083541fd1b712d892823a997af137599f56
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-04 18:30:42 +0200

    Zadanie z lekcji Operatory, konwersje typu, operacje na zmiennych git status
    q
    exit

---
+++ src/Operators/ZadanieVat.java
@@ -0,0 +1,24 @@
+package Operators;
+
+import java.util.Scanner;
+
+public class ZadanieVat {
+
+    public static void main(String[] args) {
+        double netto;
+        double brutto
+        final double vat = 0.23;
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Podaj cenę netto:");
+        netto = input.nextDouble();
+        brutto = netto + (netto * vat);
+
+        System.out.println("Przy stawce Vat równej " + vat + " kwota brutto towaru wynosi " + brutto);
+    }
+
+
+}
