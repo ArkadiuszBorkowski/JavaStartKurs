df7c0a2585aa3e832ca2c70671d6bda560453626
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-10-25 00:00:45 +0200

    Wyjątki throws

---
+++ src/Throws/Car.java
@@ -0,0 +1,29 @@
+package Throws;
+
+class Car {
+    private static final double FUEL_CONSUMPTION = 8;  // 8l//100km
+    private static final double MAX_FUEL = 50;
+    private double fuel;
+
+    public void refuel(double additionalFuel) {
+        if(fuel + additionalFuel > MAX_FUEL)
+            throw new IllegalArgumentException("Nie możesz zatankować tyle paliwa. Maksymalnie się jeszcze zmieścci");
+            //System.out.println("Nie możesz zatankować tyle paliwa. Maksymalnie się jeszcze zmieścci" + (MAX_FUEL -
+            // fuel));
+        else
+            fuel += additionalFuel;
+    }
+
+    public void drive() {
+        if( fuel - FUEL_CONSUMPTION < 0)
+            throw new IllegalStateException("Masz za mało paliwa na podróż. Zatankuj");
+            //System.out.println("Masz za mało paliwa. Zatankuj");
+        else
+            fuel -= FUEL_CONSUMPTION;
+    }
+
+    @Override
+    public String toString() {
+        return "Stan paliwa: " + fuel;
+    }
+}
---
+++ src/Throws/CarTest.java
@@ -0,0 +1,25 @@
+package Throws;
+
+class CarTest {
+    public static void main(String[] args) {
+        Car car = new Car();
+        try {
+            car.refuel(10);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println(car);
+        try {
+            car.drive();
+            car.drive();
+            car.drive();
+        } catch (IllegalStateException e) {
+            System.err.println(e.getMessage());
+        }
+
+        System.out.println(car);
+    }
+
+
+}
