078aed1356ac134e3f58f498f00045a8c3bb94ea
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-11-26 23:31:56 +0100

    Zadanie 2 Zbiory set

---
+++ src/ZbiorySet/cw2/ProductMain.java
@@ -0,0 +1,8 @@
+package ZbiorySet.cw2;
+
+class ProductMain {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        productManager.mainLoop();
+    }
+}
