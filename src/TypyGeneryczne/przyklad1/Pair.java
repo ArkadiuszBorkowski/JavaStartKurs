45e42dd729b49a6d9142eac492473c585e780d6a
ArkadiuszBorkowski <borkowskiarkadiusz91@gmail.com>
2023-12-21 20:13:25 +0100

    ćwiczenie 1 - typy generyczne

--- src/TypyGeneryczne/przyklad1/Pair.java
+++ src/TypyGeneryczne/przyklad1/Pair.java
@@ -4,6 +4,11 @@ public class Pair <T, V> {
     private T t;
     private V v;
 
+    public Pair(T t, V v) {
+        this.t = t;
+        this.v = v;
+    }
+
     public T getT() {
         return t;
     }
--- src/TypyGeneryczne/przyklad1/PairTest.java
+++ src/TypyGeneryczne/przyklad1/PairTest.java
@@ -1,4 +1,13 @@
 package TypyGeneryczne.przyklad1;
 
 public class PairTest {
+    public static void main(String[] args) {
+        Pair<String, Integer> p1 = new Pair<>("Ania", 1991);
+        Pair<Double, Double> p2 = new Pair<>(20.5, 31.7);
+        Pair<String, String> p3 = new Pair<>("Jan", "Kowalski");
+
+        System.out.println(p1.getT());
+    }
+
 }
+
